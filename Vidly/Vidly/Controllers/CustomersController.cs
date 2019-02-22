8dacf3adbe8d72aa5d796353d52e11ad36c7f840
Renan C. Lopes <renan.lopes@microsffer.com.br>
2019-02-22 08:22:25 -0300

    Adicionadas migrations para criacao do banco de dados com tabelas de customer e movies

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public List<Customer> _customers = new List<Customer>
+        {
+            new Customer{ Id = 1, Name = "John Smith"},
+            new Customer{ Id = 2, Name = "Mary Williams"}
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(_customers);
+        }
+
+        public ActionResult CustomerDetails(int Id)
+        {
+            var customer = _customers.Where(x => x.Id == Id).FirstOrDefault();
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> _movies = new List<Movie>
+        {
+            new Movie {Id = 1, Name = "Movie 1"},
+            new Movie {Id = 2, Name = "Movie 2"}
+        };
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        public ActionResult MovieDetails(int Id)
+        {
+            var movie = _movies.Where(x => x.Id == Id).FirstOrDefault();
+
+            if (movie == null)
+                return HttpNotFound();            
+
+            return View(movie);
+        }
+
+    }
+}
