a95108b88144b1ebebeab173d3deba285599ecc3
Renan C. Lopes <renan.lopes@microsffer.com.br>
2019-03-07 14:56:41 -0300

    progress saved

---
+++ Vidly/Vidly/Models/Min18YearIfMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("BirthDate is required");
+
+            var age = DateTime.Now.Year - customer.BirthDate.Value.Year;
+
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 years old to go on a Memebership");
+
+
+        }
+    }
+}
---
+++ Vidly/Vidly/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<MovieGenre> Genre { get; set; }
+    }
+}
