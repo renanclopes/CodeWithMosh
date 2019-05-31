d360e7372bf2d52fba30344fad325f46ddf0f70e
Renan C. Lopes <renan.lopes@microsffer.com.br>
2019-05-31 08:04:52 -0300

    Beginning of Section 'Building a Model Using Data-base First Workflow'

---
+++ Entity Framework 6 in Depth/DbFirst/DbFirst/DbFirst/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var author = new Author
+            {
+                Name = "Renan Camargo Lopes"
+            };
+
+            var result = await PersistAuthorsAsync(author);
+        }
+
+        public async Task<bool>  PersistAuthorsAsync(Author author)
+        {
+            var dbcontext = new PlutoDbContext();
+
+            dbcontext.Authors.Add(author);
+            await dbcontext.SaveChangesAsync();
+
+
+        }
+    }
+
+    public class PlutoDataBasePersist
+    {
+        public async Task<bool> SaveChangesAsync<T>(T entity) where T : class
+        {
+
+        }
+    }
+}
