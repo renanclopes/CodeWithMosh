a1aa8d12dba60c3f5072627df09f7e5da98a637e
Renan C. Lopes <renan.lopes@microsffer.com.br>
2019-06-10 08:21:34 -0300

    Code fisrt existing database added

---
+++ Entity Framework 6 in Depth/CodeFirstExistingDatabase/CodeFirstExistingDatabase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExistingDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+            var author = context.Authors.ToList();
+
+        }
+    }
+}
